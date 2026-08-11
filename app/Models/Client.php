2c8577b25f072be1825c0d7a9d28dc90eaa7c92a
Pedro Vidal <pedrorvidal@gmail.com>
2026-08-10 22:27:52 -0300

    update no model user

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -2,13 +2,14 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Client extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory, SoftDeletes, HasUuids;
 
     protected $fillable = [
         'user_id',
@@ -19,6 +20,11 @@ class Client extends Model
     protected $casts = [
         'birthdate' => 'datetime',
     ];
+
+    public function uniqueIds()
+    {
+        return ['uuid'];
+    }
 
     public function user()
     {
